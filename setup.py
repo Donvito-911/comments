d535791ff973fa9e5b9097068948671d172ace3c
Daniel Aguirre <danielaguirre@Daniels-MacBook-Pro.local>
2022-01-25 01:26:18 -0500

    twitter miner finished vAlpha

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.0'
+DESCRIPTION = 'Social Media miner comments'
+LONG_DESCRIPTION = 'A package that allows to recolect in databases the comments of user in facebook, instagram and twitter posts'
+
+# Setting up
+setup(
+    name="comment",
+    version=VERSION,
+    author="Donvito (Daniel Aguirre)",
+    author_email="<danielaguirre722@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['requests', 'pandas','functools'],
+    keywords=['python', 'comments', 'social media', 'twitter', 'facebook', 'instagram'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
